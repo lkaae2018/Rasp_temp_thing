4a14fccbf922b95d44de38ccac4025035280b0eb
lkaae2018 <40848476+lkaae2018@users.noreply.github.com>
2020-08-21 17:06:56 +0200

    Update CPUtemp_py3.py

--- CPUtemp_py3.py
+++ CPUtemp_py3.py
@@ -1,16 +1,16 @@
 import thingspeak
 import time
 
-channel_id = 1049013 # PUT CHANNEL ID HERE
-write_key='B530FR63JBR4S3MN' # PUT YOUR WRITE KEY HERE
-read_key='BQE5HYPZ0YTXJULT' # PUT YOUR READ KEY HERE
+channel_id = ****** # PUT CHANNEL ID HERE
+write_key='***************' # PUT YOUR WRITE KEY HERE
+read_key='***************' # PUT YOUR READ KEY HERE
 
 def thermometer(channel):
                         
     try:
         #callableculate CPU temperature of Raspberry Pi in Degrees C
         temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3 # Get Raspberry Pi CPU temp
-        params= channel.update({'field5': temp})
+        params= channel.update({'field1': temp})
         print(temp)
                 #print response.status, response.reason
                
